f578ece9cf83fcbf58d9e505b56c13fdc34ebf0e
Chi2k1 <108881879+Chi2k1@users.noreply.github.com>
2026-08-26 08:53:29 +0700

    Add argument for number of rows in data generation

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,17 @@
+import argparse
 import csv
 import random
 import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_rows", type=int, default=100)
+args = parser.parse_args()
 
 os.makedirs("/valohai/outputs", exist_ok=True)
 with open("/valohai/outputs/raw_data.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(["feature_1", "feature_2", "label"])
-    for _ in range(100):
+    for _ in range(args.num_rows):
         writer.writerow([random.random(), random.random(), random.randint(0, 1)])
 
-print("Da tao xong raw_data.csv voi 100 dong du lieu gia lap.")
+print(f"Da tao xong raw_data.csv voi {args.num_rows} dong du lieu gia lap.")
